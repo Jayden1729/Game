38ffb2bbebf5de11b422d87b198d93554a0cfe20
Jayden <jaynor4t@gmail.com>
2024-04-08 18:15:59 +0800

    Added function to attack by creating player bullets

--- Player.py
+++ Player.py
@@ -2,6 +2,8 @@
 
 """
 import pygame
+import Level
+import Bullet
 
 
 class Player(pygame.sprite.Sprite):
@@ -11,11 +13,29 @@ class Player(pygame.sprite.Sprite):
         Initialises the Player class.
         """
         super(Player, self).__init__()
-
         rect_size = 40
         self.surf = pygame.Surface((rect_size, rect_size))
         self.surf.fill((0, 0, 255))
         self.rect = self.surf.get_rect(center=(400, 400))
         self.vector2 = pygame.math.Vector2(400, 400)
-        self.speed = 7
+        self.speed = 6
         self.hp = 100
+        self.projectile_speed = 10
+        self.attack_cooldown = 0
+
+        self.grid_x = None
+        self.grid_y = None
+
+    def attack(self, player_bullets):
+        """Creates a bullet and adds to player_bullets group
+
+        Args:
+            player_bullets (pygame.Group[Bullets]): a pygame.Group object of Bullet objects.
+        """
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        mouse_vector = pygame.math.Vector2(mouse_x, mouse_y)
+
+        bullet_vector = mouse_vector - self.vector2
+
+        player_bullets.add(Bullet.Bullet(400, 400, bullet_vector, self.projectile_speed))
+        self.attack_cooldown = 20
