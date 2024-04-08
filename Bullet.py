09237ce28fc961920361c57b8b0bbaeb7107b42e
Jayden <jaynor4t@gmail.com>
2024-04-08 18:54:29 +0800

    Updated documentation of Bullet class

--- Bullet.py
+++ Bullet.py
@@ -1,13 +1,22 @@
-
 import pygame
+
 
 class Bullet(pygame.sprite.Sprite):
 
     def __init__(self, x, y, vector, speed):
+        """Initialises instance of Bullet class.
+
+        Args:
+            x (int): initial x position of bullet on screen.
+            y (int): initial y position of bullet on screen.
+            vector (pygame.math.Vector2): direction of bullet movement.
+            speed (float): speed of bullet movement.
+        """
+        super(Bullet, self).__init__()
+        self.x = x
+        self.y = y
         self.vector = vector.normalize()
         self.speed = speed
 
         self.surf = pygame.Surface((10, 10))
-        self.rect = self.surf.get_rect(center = (x, y))
-
-
+        self.rect = self.surf.get_rect(center=(x, y))
