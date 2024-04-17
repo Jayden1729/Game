d157a5da4b3850393641a7d0df92b17c989be9b9
Jayden <jaynor4t@gmail.com>
2024-04-17 22:11:33 +0800

    Added function to extract locations of each tile type within the tileset and store in a dictionary

--- Images.py
+++ Images.py
@@ -1,12 +1,61 @@
-
 import pygame
 import copy
 
+
 class Images():
 
-    def __init__(self):
+    def __init__(self, screen):
         self.tileset = pygame.image.load("sprites/TileSet.png")
-        self.tile_dict = self.generate_tile_dict
+        self.tile_dict = self.generate_tile_dict(screen)
+
+    def generate_tile_dict(self):
+        """Generates a dictionary with the locations of each type of tile.
+
+        Takes the tileset and sets self.tile_dict to a dictionary containing rects with the location of each type of
+        tile in the tileset.
+        """
+        tile_width = self.tileset.get_width() / 30
+        tile_height = self.tileset.get_height() / 17
+        print(tile_height, tile_width)
+
+        centre = pygame.Rect((11 * tile_height, 7 * tile_height), (tile_width, tile_height))
+
+        right_centre = pygame.Rect((12 * tile_height, 7 * tile_height), (tile_width, tile_height))
+
+        left_centre = pygame.Rect((10 * tile_height, 7 * tile_height), (tile_width, tile_height))
+
+        top_centre = pygame.Rect((11 * tile_height, 6 * tile_height), (tile_width, tile_height))
+
+        bot_centre = pygame.Rect((11 * tile_height, 8 * tile_height), (tile_width, tile_height))
+
+        top_right_corner = pygame.Rect((12 * tile_height, 6 * tile_height), (tile_width, tile_height))
+
+        top_left_corner = pygame.Rect((10 * tile_height, 6 * tile_height), (tile_width, tile_height))
+
+        bot_right_corner = pygame.Rect((12 * tile_height, 8 * tile_height), (tile_width, tile_height))
+
+        bot_left_corner = pygame.Rect((10 * tile_height, 8 * tile_height), (tile_width, tile_height))
+
+        vertical_centre = pygame.Rect((14 * tile_height, 7 * tile_height), (tile_width, tile_height))
+
+        horizontal_centre = pygame.Rect((17 * tile_height, 8 * tile_height), (tile_width, tile_height))
+
+        bot_end = pygame.Rect((14 * tile_height, 8 * tile_height), (tile_width, tile_height))
+
+        top_end = pygame.Rect((14 * tile_height, 6 * tile_height), (tile_width, tile_height))
+
+        right_end = pygame.Rect((18 * tile_height, 8 * tile_height), (tile_width, tile_height))
+
+        left_end = pygame.Rect((16 * tile_height, 8 * tile_height), (tile_width, tile_height))
+
+        single = pygame.Rect((16 * tile_height, 6 * tile_height), (tile_width, tile_height))
+
+        self.tile_dict = {'centre': centre, 'right_centre': right_centre, 'left_centre': left_centre,
+                          'top_centre': top_centre, 'bot_centre': bot_centre, 'top_right_corner': top_right_corner,
+                          'top_left_corner': top_left_corner, 'bot_right_corner': bot_right_corner,
+                          'bot_left_corner': bot_left_corner, 'vertical_centre': vertical_centre,
+                          'horizontal_centre': horizontal_centre, 'bot_end': bot_end, 'top_end': top_end,
+                          'right_end': right_end, 'left_end': left_end, 'single': single}
 
     def level_images(self, level):
 
@@ -19,6 +68,7 @@ class Images():
                     new_grid[j][i] = wall_type
 
         print(new_grid)
+
 
 def get_wall_type(wall, grid):
     """Determines the type of wall connector to display.
