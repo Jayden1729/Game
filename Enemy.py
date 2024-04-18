95b89cf71f2ddfbe4a1a3199c5faecbafe5e8c96
Jayden <jaynor4t@gmail.com>
2024-04-18 14:10:08 +0800

    Removed A* algorithm code, as no longer being used

--- Enemy.py
+++ Enemy.py
@@ -13,7 +13,7 @@ import Player
 
 class Enemy(pygame.sprite.Sprite):
 
-    def __init__(self, position, hp, attack_pattern = 'normal'):
+    def __init__(self, position, hp, attack_pattern='normal'):
         """Initialises an instance of the Enemy class.
 
         Args:
@@ -76,7 +76,7 @@ class Enemy(pygame.sprite.Sprite):
             cooldown (int): number of frames between attacks.
         """
 
-        degrees = 360/num_bullets
+        degrees = 360 / num_bullets
 
         if self.seen_player and self.attack_cooldown == 0:
             for i in range(num_bullets):
@@ -99,7 +99,7 @@ class Enemy(pygame.sprite.Sprite):
         """
 
         if self.seen_player and self.attack_cooldown == 0 and self.dist_to_player <= 100:
-            attack_direction = self.attack_direction.normalize()*self.rect_size
+            attack_direction = self.attack_direction.normalize() * self.rect_size
             centre_vector = pygame.math.Vector2(self.rect.x + self.rect_size / 2, self.rect.y + self.rect_size / 2)
             attack_centre = centre_vector + attack_direction
             level.enemy_melee.add(
@@ -119,12 +119,10 @@ class Enemy(pygame.sprite.Sprite):
         """
 
         if self.seen_player and self.attack_cooldown == 0 and self.dist_to_player <= 50:
-
             level.enemy_melee.add(
                 Bullet.Melee(self.rect.x, self.rect.y, 150, 150, 200))
 
             self.kill()
-
 
     def attack(self, level, cooldown):
         """Causes enemy attack based on attack pattern.
@@ -226,117 +224,3 @@ class Enemy(pygame.sprite.Sprite):
 
                 elif enemy_y < (wall_y + wall_height) < (enemy_y + enemy_height):
                     self.rect.move_ip(0, (wall_y + wall_height - enemy_y))
-
-
-class Point:
-    def __init__(self, position, parent=-1):
-        """Initialises Point class
-
-        Args:
-            position (List[int]), optional: (y, x), position of point on grid
-            parent (Point): Point from which this point was generated
-        """
-
-        self.position = position
-        self.actual_cost = 0
-        self.parent = parent
-
-        if parent != -1:
-            self.actual_cost = parent.actual_cost + 1
-
-
-def remove_strings(grid):
-    grid = copy.deepcopy(grid)
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] != 1:
-                grid[i][j] = 0
-    return grid
-
-
-def find_path(grid, start, end):
-    """Finds a path on a grid between defined start and end points.
-
-    Args:
-        grid (List[List[int]]): 2D list of 0's and 1's. Element int(1) is treated as a wall.
-        start (Point): start point.
-        end (Point): end point.
-
-    Returns:
-        List[List[int]]: A 2D list of values. 4 denotes the start point, 2 denotes points along the path, 5 denotes the
-        end point, 3 denotes points that were searched, and 1 denotes walls.
-
-    Raises:
-        Exception "NO POSSIBLE PATH": if there is no possible path between the start and end points.
-    """
-
-    to_search = [start]
-    searched = np.zeros(np.shape(grid))
-
-    finished = False
-
-    while not finished:
-
-        lowest_cost = -1
-
-        for point in to_search:
-
-            estimated_cost = abs(end.position[0] - point.position[0]) + abs(end.position[1] - point.position[1])
-            cost = point.actual_cost + 1.2 * estimated_cost
-
-            if lowest_cost == -1 or lowest_cost > cost:
-                lowest_cost = cost
-                lowest_point = point
-
-        low_x = lowest_point.position[0]
-        low_y = lowest_point.position[1]
-
-        searched[low_x, low_y] = 3
-        to_search.remove(lowest_point)
-
-        grid_dim = np.shape(searched)
-
-        new_points = [Point([low_x - 1, low_y], lowest_point),
-                      Point([low_x, low_y - 1], lowest_point),
-                      Point([low_x + 1, low_y], lowest_point),
-                      Point([low_x, low_y + 1], lowest_point)]
-
-        adjacent_points = []
-        for point in new_points:
-            x = point.position[0]
-            y = point.position[1]
-
-            if x < 0 or x > (grid_dim[0] - 1) or y < 0 or y > (grid_dim[1] - 1):
-                continue
-
-            if grid[x][y] == 1 or searched[x][y] == 3:
-                continue
-
-            if point.position == end.position:
-                adjacent_points = [point]
-                finished = True
-
-            else:
-                adjacent_points.append(point)
-
-        to_search.extend(adjacent_points)
-
-        if len(to_search) == 0:
-            print(grid, start.position, end.position)
-            raise Exception("NO POSSIBLE PATH")
-
-    current_point = adjacent_points[0]
-    parent = current_point.parent
-
-    while parent != -1:
-        searched[current_point.position[0]][current_point.position[1]] = 2
-        current_point = parent
-        parent = current_point.parent
-
-    searched[start.position[0]][start.position[1]] = 4
-    searched[end.position[0]][end.position[1]] = 5
-
-    searched = np.asarray(searched)
-    grid = np.asarray(grid)
-
-    return searched + grid
